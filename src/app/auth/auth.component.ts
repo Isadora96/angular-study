8bef5fdbae977cff67e73497a6dfcb3bad2cfbc8
Isadora Lopes <80006401+Isadora96@users.noreply.github.com>
2023-07-30 13:36:16 -0300

    Add files via upload

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,68 +1,97 @@
-import { Component, ViewContainerRef } from "@angular/core";
-import { NgForm } from "@angular/forms";
-import { AuthService, AuthResponseData } from "./auth.service";
-import { Observable, Subscription } from "rxjs";
-import { Router } from "@angular/router";
-import { AlertComponent } from "../shared/alert/alert.component";
+import {
+  Component,
+  ComponentFactoryResolver,
+  ViewChild,
+  OnDestroy
+} from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
 
+import { AuthService, AuthResponseData } from './auth.service';
+import { AlertComponent } from '../shared/alert/alert.component';
+import { PlaceholderDirective } from '../shared/placeholder/placeholder.directive';
 
 @Component({
-    selector: 'app-auth',
-    templateUrl: './auth.component.html'
+  selector: 'app-auth',
+  templateUrl: './auth.component.html'
 })
-export class AuthComponent {
+export class AuthComponent implements OnDestroy {
+  isLoginMode = true;
+  isLoading = false;
+  error: string = null;
+  @ViewChild(PlaceholderDirective, { static: false }) alertHost: PlaceholderDirective;
 
-    isLoginMode: boolean = true;
-    isLoading: boolean = false;
-    errorMessage: string | null = null;
+  private closeSub: Subscription;
 
-    private closeSub!: Subscription;
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private componentFactoryResolver: ComponentFactoryResolver
+  ) {}
 
-    constructor (private authService: AuthService, private router: Router, private viewContainerRef: ViewContainerRef) {}
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
 
-    onSwitchMode() {
-        this.isLoginMode = !this.isLoginMode;
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+
+    let authObs: Observable<AuthResponseData>;
+
+    this.isLoading = true;
+
+    if (this.isLoginMode) {
+      authObs = this.authService.login(email, password);
+    } else {
+      authObs = this.authService.signup(email, password);
     }
 
-    onSubmit(form: NgForm) {
-        let authObs: Observable<AuthResponseData>;
+    authObs.subscribe(
+      resData => {
+        console.log(resData);
+        this.isLoading = false;
+        this.router.navigate(['/recipes']);
+      },
+      errorMessage => {
+        console.log(errorMessage);
+        this.error = errorMessage;
+        this.showErrorAlert(errorMessage);
+        this.isLoading = false;
+      }
+    );
 
-        if(!form.valid) return
+    form.reset();
+  }
 
-        const email = form.value.email;
-        const password = form.value.password;
-        this.isLoading = true;
+  onHandleError() {
+    this.error = null;
+  }
 
-        if(this.isLoginMode) {
-            authObs = this.authService.login(email, password);
-        } else {
-            authObs = this.authService.signup(email, password);
-        }
-
-        authObs.subscribe(resData => {
-            this.isLoading = false;
-            this.router.navigate(['./recipes']);
-        }, error => {
-            this.errorMessage = error,
-            this.isLoading = false;
-            this.showErrorAlert(this.errorMessage);
-        })
-
-        form.reset();
+  ngOnDestroy() {
+    if (this.closeSub) {
+      this.closeSub.unsubscribe();
     }
+  }
 
-    onHandleError() {
-        this.errorMessage = null;
-    }
+  private showErrorAlert(message: string) {
+    // const alertCmp = new AlertComponent();
+    const alertCmpFactory = this.componentFactoryResolver.resolveComponentFactory(
+      AlertComponent
+    );
+    const hostViewContainerRef = this.alertHost.viewContainerRef;
+    hostViewContainerRef.clear();
 
-    private showErrorAlert(message: string | null) {
-        const alertCpmFactory = this.viewContainerRef.createComponent(AlertComponent);
+    const componentRef = hostViewContainerRef.createComponent(alertCmpFactory);
 
-        alertCpmFactory.instance.message = message!;
-        this.closeSub = alertCpmFactory.instance.close.subscribe(() => {
-            this.closeSub.unsubscribe();
-            this.viewContainerRef.clear();
-        });
-        
-    }
+    componentRef.instance.message = message;
+    this.closeSub = componentRef.instance.close.subscribe(() => {
+      this.closeSub.unsubscribe();
+      hostViewContainerRef.clear();
+    });
+  }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,109 +1,161 @@
-import { HttpClient, HttpErrorResponse } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { BehaviorSubject, catchError, tap, throwError } from "rxjs";
-import { User } from "./user.model";
-import { Router } from "@angular/router";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { catchError, tap } from 'rxjs/operators';
+import { throwError, BehaviorSubject } from 'rxjs';
 import { environment } from '../../environments/environment';
 
+import { User } from './user.model';
+import { Store } from '@ngrx/store';
+
+import * as fromApp from '../store/app.reducer';
+import * as AuthActions from './store/auth.actions';
+
 export interface AuthResponseData {
-    idToken: string,
-    email: string,
-    refreshToken: string,
-    expiresIn: string,
-    localId: string,
-    registered?: boolean
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
 }
 
-@Injectable({
-    providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class AuthService {
+  // user = new BehaviorSubject<User>(null);
+  private tokenExpirationTimer: any;
 
-    user = new BehaviorSubject<User>(null!);
+  constructor(private http: HttpClient, private router: Router, private store: Store<fromApp.AppState>) {}
 
-    private tokenExpirationTimer: any;
-
-    constructor(private http: HttpClient, private router: Router) {}
-
-
-    signup(email: string, password: string) {
-
-        const url: string = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + environment.firebaseAPIKey;
-        const data = { email, password, returnSecureToken: true}
-
-        return this.http.post<AuthResponseData>(url, data)
-        .pipe(catchError(this.handleError), tap(resData => {
-            this.handleAuth(resData.email, resData.localId, resData.idToken, +resData.expiresIn)
-        }));
-    }
-
-    login(email: string, password: string) {
-        const url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + environment.firebaseAPIKey;
-        const data = { email, password, returnSecureToken: true}
-
-        return this.http.post<AuthResponseData>(url, data)
-        .pipe(catchError(this.handleError), tap(resData => {
-            this.handleAuth(resData.email, resData.localId, resData.idToken, +resData.expiresIn)
-        }));
-    }
-
-    logout() {
-        this.user.next(null!);
-        this.router.navigate(['/auth']);
-        localStorage.removeItem('userData');
-        if(this.tokenExpirationTimer) {
-            clearTimeout(this.tokenExpirationTimer);
+  signup(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=' + environment.firebaseAPIKey,
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true
         }
+      )
+      .pipe(
+        catchError(this.handleError),
+        tap(resData => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
+        })
+      );
+  }
 
-        this.tokenExpirationTimer = null;
-    }
-
-    autoLogout(expirationDuration: number) {
-        this.tokenExpirationTimer = setTimeout(() => {
-            this.logout();
-        }, expirationDuration);
-    }
-
-    autoLogin() {
-        const userData: {
-            email: string,
-            id: string,
-            _token: string,
-            _tokenExpirationDate: string;
-        } = JSON.parse(localStorage.getItem('userData')!);
-
-        if(userData) {
-            const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
-            if(loadedUser.token) {
-                this.user.next(loadedUser);
-                const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
-                this.autoLogout(expirationDuration);
-            }
+  login(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=' + environment.firebaseAPIKey,
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true
         }
+      )
+      .pipe(
+        catchError(this.handleError),
+        tap(resData => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
+        })
+      );
+  }
+
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) {
+      return;
     }
 
-    private handleAuth(email: string, userId: string, token: string, expiresIn: number) {
-        const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
-        const user = new User(email, userId, token, expirationDate);
-        this.user.next(user);
-        this.autoLogout(expiresIn * 1000);
-        localStorage.setItem('userData', JSON.stringify(user));
-    }
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    );
 
-    private handleError(errorResp: HttpErrorResponse) {
-        let errorMessage = 'An unknown error occurred!';
-        if(!errorResp.error || !errorResp.error.error) {
-            return  throwError(errorMessage);
-        }
-        switch (errorResp.error.error.message) {
-            case 'EMAIL_EXISTS':
-                errorMessage = 'This email exists already';
-                break;
-            case 'EMAIL_NOT_FOUND' || 'INVALID_PASSWORD':
-                errorMessage = 'Email or password incorrect!'
-                break;
-        }
-
-        return throwError(errorMessage);
+    if (loadedUser.token) {
+      this.store.dispatch(new AuthActions.Login({
+        email: loadedUser.email,
+        userId: loadedUser.id,
+        token: loadedUser.token,
+        expirationDate: new Date(userData._tokenExpirationDate)
+      }))
+      const expirationDuration =
+        new Date(userData._tokenExpirationDate).getTime() -
+        new Date().getTime();
+      this.autoLogout(expirationDuration);
     }
+  }
+
+  logout() {
+    this.store.dispatch(new AuthActions.Logout());
+    this.router.navigate(['/auth']);
+    localStorage.removeItem('userData');
+    if (this.tokenExpirationTimer) {
+      clearTimeout(this.tokenExpirationTimer);
+    }
+    this.tokenExpirationTimer = null;
+  }
+
+  autoLogout(expirationDuration: number) {
+    this.tokenExpirationTimer = setTimeout(() => {
+      this.logout();
+    }, expirationDuration);
+  }
+
+  private handleAuthentication(
+    email: string,
+    userId: string,
+    token: string,
+    expiresIn: number
+  ) {
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+    const user = new User(email, userId, token, expirationDate);
+    this.store.dispatch(new AuthActions.Login({
+      email,
+      userId,
+      token,
+      expirationDate
+    }));
+    this.autoLogout(expiresIn * 1000);
+    localStorage.setItem('userData', JSON.stringify(user));
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occurred!';
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email exists already';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'This email does not exist.';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'This password is not correct.';
+        break;
+    }
+    return throwError(errorMessage);
+  }
 }
