ccb888497d7ab1e93bb6d2f4aff5103a7a34607e
Isadora Lopes <80006401+Isadora96@users.noreply.github.com>
2023-07-30 13:45:15 -0300

    Add files via upload

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,25 +1,12 @@
-import { Directive, ElementRef, Renderer2, OnInit, HostListener, HostBinding } from '@angular/core';
+import { Directive, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
-    selector: '[appDropdown]'
+  selector: '[appDropdown]'
 })
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen = false;
 
-export class DropdownDirective implements OnInit {
-    @HostBinding('class.open') isOpen = false;
-    //my solution
-	// constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
-
-    ngOnInit() {
-        
-    }
-
-    // @HostListener('click') mouseclick(eventData: Event) {
-    //     this.elementRef.nativeElement.className.includes('open') ?
-    //     this.renderer.removeClass(this.elementRef.nativeElement, 'open') :
-    //     this.renderer.addClass(this.elementRef.nativeElement, 'open');
-    // }
-
-    @HostListener('click') toggleOpen() {
-        this.isOpen = !this.isOpen;
-    }
+  @HostListener('click') toggleOpen() {
+    this.isOpen = !this.isOpen;
+  }
 }
